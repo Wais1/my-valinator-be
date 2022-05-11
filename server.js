0a809e5a55f3214233e8805b993b9cb2bfdbf2cf
Wais1 <waisibrahim77@gmail.com>
2022-05-11 16:43:57 +0700

    test2

--- server.js
+++ server.js
@@ -93,6 +93,7 @@ io.on('connection', socket => {
     })
 })
 
-const PORT = 5000 || process.env.PORT;
+const PORT = 5000 
+//|| process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
